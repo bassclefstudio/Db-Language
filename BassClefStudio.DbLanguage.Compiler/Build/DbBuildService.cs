38716cf7e2ffff221150866352d5f10d981bd594
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-04 11:50:49 -0400

    Removed legacy Lifecycle components and dependent build classes.

--- BassClefStudio.DbLanguage.Compiler/Build/DbBuildService.cs
+++ BassClefStudio.DbLanguage.Compiler/Build/DbBuildService.cs
@@ -1,7 +1,6 @@
 ﻿using BassClefStudio.DbLanguage.Compiler.Parse;
 using BassClefStudio.DbLanguage.Core.Data;
 using BassClefStudio.DbLanguage.Core.Documentation;
-using BassClefStudio.DbLanguage.Core.Lifecycle;
 using BassClefStudio.DbLanguage.Core.Memory;
 using BassClefStudio.DbLanguage.Core.Runtime.Info;
 using BassClefStudio.DbLanguage.Core.Runtime.Scripts;
@@ -16,152 +15,152 @@ namespace BassClefStudio.DbLanguage.Compiler.Build
     /// <summary>
     /// An <see cref="IBuildService"/> for building, type-checking, compiling, and creating <see cref="ILibrary"/> instances from <see cref="StringLibrary"/> objects created by a relevant <see cref="IParseService"/>.
     /// </summary>
-    internal class DbBuildService : IBuildService
-    {
-        /// <inheritdoc/>
-        public ILibrary Build(StringLibrary stringLib, IEnumerable<ILibrary> dependencies)
-        {
-            IEnumerable<ILibrary> usedDependencies = dependencies
-                .Where(l => stringLib.Dependencies.Contains((string)l.Name));
+    //internal class DbBuildService : IBuildService
+    //{
+    //    /// <inheritdoc/>
+    //    public ILibrary Build(StringLibrary stringLib, IEnumerable<ILibrary> dependencies)
+    //    {
+    //        IEnumerable<ILibrary> usedDependencies = dependencies
+    //            .Where(l => stringLib.Dependencies.Contains((string)l.Name));
 
-            CompilerLibrary lib = new CompilerLibrary();
+    //        CompilerLibrary lib = new CompilerLibrary();
 
-            lib.DependentLibraries = usedDependencies;
-            lib.Name = stringLib.Name;
+    //        lib.DependentLibraries = usedDependencies;
+    //        lib.Name = stringLib.Name;
 
-            //Create pairs of string/Db types with names.
-            IEnumerable<TypeBuilder> pairs = stringLib.DefinedTypes.Select(s => new TypeBuilder(s));
-            IDictionary<Namespace, IType> types = pairs.Select(p => p.DbType).Concat(usedDependencies.SelectMany(d => d.Definitions)).ToDictionary(t => t.TypeName);
+    //        //Create pairs of string/Db types with names.
+    //        IEnumerable<TypeBuilder> pairs = stringLib.DefinedTypes.Select(s => new TypeBuilder(s));
+    //        IDictionary<Namespace, IType> types = pairs.Select(p => p.DbType).Concat(usedDependencies.SelectMany(d => d.Definitions)).ToDictionary(t => t.TypeName);
 
-            //Iterate through the headers of TypePairs, creating parent/child structure and inheritance.
-            foreach (var p in pairs)
-            {
-                if (p.DbType is DataContract contract)
-                {
-                    foreach (var d in p.StringType.Header.Dependencies)
-                    {
-                        if(types.TryGetValue(d, out var dependency))
-                        {
-                            if(dependency is DataContract dC)
-                            {
-                                contract.InheritedContracts.Add(dC);
-                            }
-                            else
-                            {
-                                throw new BuildException($"A DataContract cannot inherit from a concrete type. (contract: {p.Name}, inheriting: {dependency.TypeName})");
-                            }
-                        }
-                        else
-                        {
-                            throw new BuildException($"DataContract could not find inherited contract with name {d}. (contract: {p.Name})");
-                        }
-                    }
-                }
-                else if (p.DbType is DataType type)
-                {
-                    foreach (var d in p.StringType.Header.Dependencies)
-                    {
-                        if (types.TryGetValue(d, out var dependency))
-                        {
-                            if (dependency is DataContract dC)
-                            {
-                                type.InheritedContracts.Add(dC);
-                            }
-                            else if (dependency is DataType dT)
-                            {
-                                if(type.ParentType == null)
-                                {
-                                    type.ParentType = dT;
-                                }
-                                else
-                                {
-                                    throw new BuildException($"A DataType can only have a single DataType parent. (type: {p.Name}, inheriting: {dependency.TypeName})");
-                                }
-                            }
-                            else
-                            {
-                                throw new BuildException($"A DataType only inherits from DataTypes or DataContracts. (type: {p.Name}, inheriting: {dependency.TypeName})");
-                            }
-                        }
-                        else
-                        {
-                            throw new BuildException($"DataType could not find inherited contract or parent type with name {d}. (type: {p.Name})");
-                        }
-                    }
-                }
-            }
+    //        //Iterate through the headers of TypePairs, creating parent/child structure and inheritance.
+    //        foreach (var p in pairs)
+    //        {
+    //            if (p.DbType is DataContract contract)
+    //            {
+    //                foreach (var d in p.StringType.Header.Dependencies)
+    //                {
+    //                    if(types.TryGetValue(d, out var dependency))
+    //                    {
+    //                        if(dependency is DataContract dC)
+    //                        {
+    //                            contract.InheritedContracts.Add(dC);
+    //                        }
+    //                        else
+    //                        {
+    //                            throw new BuildException($"A DataContract cannot inherit from a concrete type. (contract: {p.Name}, inheriting: {dependency.TypeName})");
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        throw new BuildException($"DataContract could not find inherited contract with name {d}. (contract: {p.Name})");
+    //                    }
+    //                }
+    //            }
+    //            else if (p.DbType is DataType type)
+    //            {
+    //                foreach (var d in p.StringType.Header.Dependencies)
+    //                {
+    //                    if (types.TryGetValue(d, out var dependency))
+    //                    {
+    //                        if (dependency is DataContract dC)
+    //                        {
+    //                            type.InheritedContracts.Add(dC);
+    //                        }
+    //                        else if (dependency is DataType dT)
+    //                        {
+    //                            if(type.ParentType == null)
+    //                            {
+    //                                type.ParentType = dT;
+    //                            }
+    //                            else
+    //                            {
+    //                                throw new BuildException($"A DataType can only have a single DataType parent. (type: {p.Name}, inheriting: {dependency.TypeName})");
+    //                            }
+    //                        }
+    //                        else
+    //                        {
+    //                            throw new BuildException($"A DataType only inherits from DataTypes or DataContracts. (type: {p.Name}, inheriting: {dependency.TypeName})");
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        throw new BuildException($"DataType could not find inherited contract or parent type with name {d}. (type: {p.Name})");
+    //                    }
+    //                }
+    //            }
+    //        }
 
-            foreach (var p in pairs)
-            {
-                foreach (var child in p.StringType.Properties)
-                {
-                    if (child is StringProperty prop)
-                    {
-                        if (types.TryGetValue(prop.Type, out var type))
-                        {
-                            MemoryProperty property = new MemoryProperty(prop.Name, type);
-                            if(prop.IsPublic)
-                            {
-                                p.PublicProperties.Add(property);
-                            }
-                            else
-                            {
-                                p.PrivateProperties.Add(property);
-                            }
-                        }
-                        else
-                        {
-                            throw new BuildException($"Could not find type {prop.Type} for property {p.Name}.{child.Name}.");
-                        }
-                    }
-                    else if (child is StringScript stringScript)
-                    {
-                        if (p.DbType is DataType dataType)
-                        {
-                            if (types.TryGetValue(stringScript.ReturnType, out var type))
-                            {
-                                //IEnumerable<ScriptInput> inputs = stringScript.Inputs.Select(i => new ScriptInput(i.Name, types[i.Type]));
-                                //ScriptInfo scriptInfo = new ScriptInfo(stringScript.Name, type, inputs.ToArray());
+    //        foreach (var p in pairs)
+    //        {
+    //            foreach (var child in p.StringType.Properties)
+    //            {
+    //                if (child is StringProperty prop)
+    //                {
+    //                    if (types.TryGetValue(prop.Type, out var type))
+    //                    {
+    //                        MemoryProperty property = new MemoryProperty(prop.Name, type);
+    //                        if(prop.IsPublic)
+    //                        {
+    //                            p.PublicProperties.Add(property);
+    //                        }
+    //                        else
+    //                        {
+    //                            p.PrivateProperties.Add(property);
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        throw new BuildException($"Could not find type {prop.Type} for property {p.Name}.{child.Name}.");
+    //                    }
+    //                }
+    //                else if (child is StringScript stringScript)
+    //                {
+    //                    if (p.DbType is DataType dataType)
+    //                    {
+    //                        if (types.TryGetValue(stringScript.ReturnType, out var type))
+    //                        {
+    //                            //IEnumerable<ScriptInput> inputs = stringScript.Inputs.Select(i => new ScriptInput(i.Name, types[i.Type]));
+    //                            //ScriptInfo scriptInfo = new ScriptInfo(stringScript.Name, type, inputs.ToArray());
 
-                                //Func<DataObject, Script> scriptBuilder =
-                                //    o => new Script(o, scriptInfo, BuildCommands(stringScript.Commands));
+    //                            //Func<DataObject, Script> scriptBuilder =
+    //                            //    o => new Script(o, scriptInfo, BuildCommands(stringScript.Commands));
 
-                                //Func<DataObject, DataObject> createScriptObject =
-                                //    me =>
-                                //    {
-                                //        var o = new DataObject(types["Core.Script"] as DataType);
-                                //        o.TrySetObject<Script>(scriptBuilder(me));
-                                //        return o;
-                                //    };
+    //                            //Func<DataObject, DataObject> createScriptObject =
+    //                            //    me =>
+    //                            //    {
+    //                            //        var o = new DataObject(types["Core.Script"] as DataType);
+    //                            //        o.TrySetObject<Script>(scriptBuilder(me));
+    //                            //        return o;
+    //                            //    };
 
-                                //dataType.Constructors.Add(new SetCommand(stringScript.Name, new ValueCommand(createScriptObject)));
-                            }
-                            else
-                            {
-                                throw new BuildException($"Could not find return type {stringScript.ReturnType} for script {p.Name}.{child.Name}.");
-                            }
-                        }
-                        else
-                        {
-                            throw new BuildException($"Only DataTypes may contain script blocks. (type: {p.Name})");
-                        }
-                    }
-                    else
-                    {
-                        throw new BuildException($"Property {p.Name}.{child.Name} is of unknown type or is null.");
-                    }
-                }
-            }
+    //                            //dataType.Constructors.Add(new SetCommand(stringScript.Name, new ValueCommand(createScriptObject)));
+    //                        }
+    //                        else
+    //                        {
+    //                            throw new BuildException($"Could not find return type {stringScript.ReturnType} for script {p.Name}.{child.Name}.");
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        throw new BuildException($"Only DataTypes may contain script blocks. (type: {p.Name})");
+    //                    }
+    //                }
+    //                else
+    //                {
+    //                    throw new BuildException($"Property {p.Name}.{child.Name} is of unknown type or is null.");
+    //                }
+    //            }
+    //        }
 
-            lib.Definitions = pairs.Select(p => p.DbType);
-            return lib;
-        }
+    //        lib.Definitions = pairs.Select(p => p.DbType);
+    //        return lib;
+    //    }
 
-        //private IEnumerable<ICommand> BuildCommands(IEnumerable<ICodeStatement> statements)
-        //{
-        //    throw new NotImplementedException();
-        //}
-    }
+    //    private IEnumerable<ICommand> BuildCommands(IEnumerable<ICodeStatement> statements)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 
     internal class TypeBuilder
     {
