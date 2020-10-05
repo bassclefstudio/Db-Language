3cf995b92eaac390e11375c56f1598be4fb79e5c
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-04 22:46:18 -0400

    Added SystemProcess to handle the running tasks on a system.

--- BassClefStudio.DbLanguage.Core/Lifecycle/ISystem.cs
+++ BassClefStudio.DbLanguage.Core/Lifecycle/ISystem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BassClefStudio.NET.Core;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,5 +23,10 @@ namespace BassClefStudio.DbLanguage.Core.Lifecycle
         /// </summary>
         /// <param name="info">The <see cref="PackageInfo"/> specifying the <see cref="IPackage"/> to remove.</param>
         Task RemovePackageAsync(PackageInfo info);
+
+        /// <summary>
+        /// A read-only dynamic collection of the running <see cref="SystemProcess"/>es containing the relevant <see cref="PackageInfo"/>s and their <see cref="ProcessStatus"/>.
+        /// </summary>
+        ReadOnlyObservableCollection<SystemProcess> RunningProcesses { get; }
     }
 }
