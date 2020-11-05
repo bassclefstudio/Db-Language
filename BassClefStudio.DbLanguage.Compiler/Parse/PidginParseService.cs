635726a9ef7593d95a2c81d188d4fbd12483929a
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-11-05 11:53:09 -0500

    Edits to model, including addition of source position.

--- BassClefStudio.DbLanguage.Compiler/Parse/PidginParseService.cs
+++ BassClefStudio.DbLanguage.Compiler/Parse/PidginParseService.cs
@@ -14,10 +14,10 @@ namespace BassClefStudio.DbLanguage.Compiler.Parse
     /// </summary>
     internal class PidginParseService
     {
-        //private Parser<char, StringPackage> LibraryParser { get; }
+        private Parser<char, TokenType> TypeParser { get; set; }
 
+        #region Basic
         #region Symbols
-
         private readonly Parser<char, char> OpenBrace = Char('{');
         private readonly Parser<char, char> CloseBrace = Char('}');
         private readonly Parser<char, char> OpenBracket = Char('[');
@@ -30,13 +30,101 @@ namespace BassClefStudio.DbLanguage.Compiler.Parse
         private readonly Parser<char, char> Colon = Char(':');
         private readonly Parser<char, char> SemiColon = Char(';');
         private readonly Parser<char, char> Equal = Char('=');
-
         #endregion
         #region Keywords
-
         private readonly Parser<char, string> Comment = String("//");
+        private readonly Parser<char, string> Private = String("private");
+        private readonly Parser<char, string> Public = String("public");
+        private readonly Parser<char, string> Type = String("type");
+        private readonly Parser<char, string> Contract = String("contract");
+        private readonly Parser<char, string> Var = String("var");
+        private readonly Parser<char, string> Return = String("return");
+        #endregion
+        #region Structures
+        private Parser<char, string> String;
+        private Parser<char, string> Path;
+        private Parser<char, string> Name;
+
+        private Parser<char, T> Block<T>(Parser<char, T> inner)
+        {
+            return inner.Between(OpenBrace.Between(SkipWhitespaces), CloseBrace.Between(SkipWhitespaces));
+        }
+
+        private void InitStructures()
+        {
+            Path =
+                from a in Letter
+                from b in LetterOrDigit.Or(Dot).ManyString()
+                select string.Concat(a, b);
+            Name =
+                from a in Letter
+                from b in LetterOrDigit.ManyString()
+                select string.Concat(a, b);
+            String = AnyCharExcept('"').ManyString().Between(Quote);
+        }
 
         #endregion
+        #endregion
+        #region Language
+        #region Scripts
+
+        private void InitScripts()
+        {
+
+        }
+
+        #endregion
+        #region Properties
+        private Parser<char, TokenChild> Property;
+
+        private void InitProperties()
+        {
+            // TODO: Initial value?
+            Property =
+                from p in CurrentPos
+                from v in OneOf(Try(Public.ThenReturn(true)), Try(Private.ThenReturn(false))).Before(Whitespace.AtLeastOnce()).Optional()
+                from t in Path.Before(Whitespace.AtLeastOnce())
+                from n in Name.Before(SemiColon)
+                select new TokenProperty() { SourcePosition = new TokenPos(p), IsPublic = v.GetValueOrDefault(false), ValueType = t, Name = n } as TokenChild;
+        }
+
+        #endregion
+        #region Core
+
+        private Parser<char, TokenTypeHeader> Header;
+        private Parser<char, IEnumerable<TokenChild>> Body;
+
+        private void InitLanguage()
+        {
+            Header =
+                from p in CurrentPos
+                from t in OneOf(Try(Type.ThenReturn(true)), Contract.ThenReturn(false)).Before(Whitespace.AtLeastOnce())
+                from n in Name
+                from d in Try(Colon.Between(SkipWhitespaces).Then(Path.SeparatedAtLeastOnce(Comma.Then(SkipWhitespaces)))).Optional()
+                select new TokenTypeHeader() { SourcePosition = new TokenPos(p), Name = n, IsConcrete = t, InheritsFrom = d.GetValueOrDefault(new string[0]) };
+
+            //// Add OneOf(Try(Property), Script) when scripts are ready.
+            Body = OneOf(Try(Property)).Before(SkipWhitespaces).Many();
+
+            TypeParser =
+                from h in Header
+                from b in Block(Body)
+                select new TokenType() { Header = h, Children = b };
+        }
+
+        #endregion
+        #endregion
+
+        /// <summary>
+        /// Creates a new <see cref="PidginParseService"/>, initializing the language parsing frameworks.
+        /// </summary>
+        public PidginParseService()
+        {
+            InitStructures();
+            InitScripts();
+            InitProperties();
+            InitLanguage();
+        }
     }
 
     /// <summary>
--- BassClefStudio.DbLanguage.Compiler/Parse/TokenPackages.cs
+++ BassClefStudio.DbLanguage.Compiler/Parse/TokenPackages.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Pidgin;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BassClefStudio.DbLanguage.Compiler.Parse
 {
-    internal class TokenPackage
+    internal class TokenPos
     {
+        public int LineNumber { get; }
+        public int ColumnNumber { get; }
+
+        public TokenPos(SourcePos sourcePos)
+        {
+            LineNumber = sourcePos.Line;
+            ColumnNumber = sourcePos.Col;
+        }
+    }
+
+    internal class TokenChild
+    {
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+        public TokenPos SourcePosition { get; set; }
+    }
+
+    internal class TokenTypeHeader : TokenChild
+    {
+        public bool IsConcrete { get; set; }
+        public IEnumerable<string> InheritsFrom { get; set; }
+    }
+
+    internal class TokenType
+    {
+        public TokenTypeHeader Header { get; set; }
+        public IEnumerable<TokenChild> Children { get; set; }
+    }
+
+    internal class TokenProperty : TokenChild
+    {
+        public string ValueType { get; set; }
+    }
+
+    internal class TokenScript : TokenChild
+    {
+        public string ReturnType { get; set; }
     }
 }
